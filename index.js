07cdff97daca303aef794b5d04bfa1b04e2d5683
John Tuttle <thejwt@gmail.com>
2017-09-16 09:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function findBy(array, findFn){
   var value = findFn
   for(var i = 0; i < (array.length); i ++){
     if(array[i] === findFn){
-      return findFn
+      return value
     }
   }
 }
