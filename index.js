a7573fdce93fef3d460076c5c658885af2319599
John Tuttle <thejwt@gmail.com>
2017-09-16 09:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ function keepGoing(array,changeValue,skipValue){
 function findBy(array, findFn){
   for(var i = 0; i < (array.length); i ++){
     if(array[i] === findFn){
-      return array[i];
+      var value = findFn
+      return value
     }
   }
 
